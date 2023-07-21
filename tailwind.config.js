0282ea959c8c73ee5fa9194293d1033b3438ef00
novtech06 <mn5475081@gmail.com>
2023-07-21 22:29:00 +0700

    fix masalah font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,jsx,ts,tsx}',
-		'./components/**/*.{js,jsx,ts,tsx}',
-	],
-	darkMode: 'class',
-	theme: {
-		container: {
-			center: true,
-		},
-		extend: {
-			colors: {
-				primary: '#0ea5e9',
-				dark: '#0f172a',
-				moon: '#f1c40f',
-				brightness: '#f39c12',
-				light: '#94a3b8',
-			},
-			backgroundImage: {
-				naufal: "url('../public/naufal.png')",
-			},
-			fontFamily: {
-				burtons: 'burtons',
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        dark: "#0f172a",
+        moon: "#f1c40f",
+        brightness: "#f39c12",
+        light: "#94a3b8",
+      },
+      backgroundImage: {
+        naufal: "url('../public/naufal.webp')",
+      },
+      fontFamily: {
+        burtons: "burtons",
+      },
+    },
+  },
+  plugins: [],
 };
